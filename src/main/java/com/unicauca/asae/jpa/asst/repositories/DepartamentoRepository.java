2124bb7d118eb982db9ce451834a8b7bf45bbe44
José Gregorio <108169866+jgnarvaez@users.noreply.github.com>
2024-04-15 15:38:27 -0500

    carpeta repositories

---
+++ src/main/java/com/unicauca/asae/jpa/asst/repositories/DepartamentoRepository.java
@@ -0,0 +1,8 @@
+package com.unicauca.asae.jpa.asst.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.unicauca.asae.jpa.asst.models.Departamento;
+
+public interface DepartamentoRepository extends CrudRepository<Departamento, Integer>{
+
+}
