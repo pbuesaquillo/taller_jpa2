667a73b616ebf1b0285488ab44da1f6058b505b0
pbuesaquillo <51162785+pbuesaquillo@users.noreply.github.com>
2024-04-15 15:58:22 -0500

    Interfaces repository agragadas
    interfaces para los metodos crud agregadas

---
+++ src/main/java/com/unicauca/asae/jpa/asst/repositories/DocenteRepository.java
@@ -0,0 +1,9 @@
+package com.unicauca.asae.jpa.asst.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.unicauca.asae.jpa.asst.models.Docente;
+
+public interface DocenteRepository extends CrudRepository<Docente, Integer>{
+
+}
---
+++ src/main/java/com/unicauca/asae/jpa/asst/repositories/TipoPreguntaRepository.java
@@ -0,0 +1,9 @@
+package com.unicauca.asae.jpa.asst.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.unicauca.asae.jpa.asst.models.TipoPregunta;
+
+public interface TipoPreguntaRepository extends CrudRepository<TipoPregunta, Integer>{
+
+}
